abeb7bba0d212f217f8e5b551e8ea7028df15120
MaheshM <maheshm77@gmail.com>
2018-12-25 18:06:48 +0530

    Additional TP samples

--- PScripts/Func.py
+++ PScripts/Func.py
@@ -6,5 +6,22 @@ import Timesample
 print("Current time from Func is ", Timesample.getCurrTime())
 
 
-str = input("Enter your input: ")
-print ("Received input is : ", str)
+#str = input("Enter your input: ")
+#print ("Received input is : ", str)
+
+myFile = open("/Users/maheshmachapalli/MaheshDocs/PScripts/AdvPyScrpts/GITCMNDS", "r+")
+
+print ( myFile.read(10) )
+
+myFile.write("pull, add, commit\npush")
+import os
+os.rename("/Users/maheshmachapalli/MaheshDocs/PScripts/AdvPyScrpts/GITCMNDS","/Users/maheshmachapalli/MaheshDocs/PScripts/AdvPyScrpts/GITCMNDS2")
+myFile.close()
+print (myFile.closed)
+print (myFile.name)
+print (myFile.newlines)
+print (myFile.mode)
+
+
+
+
